d56993db0edc1116b3f3fe8fc4674d3768f1d860
pranjalnigam1 <pranjalnigam1234@gmail.com>
2022-07-27 13:53:41 +0530

    Js file is now enable

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 
+
+ window.addEventListener("DOMContentLoaded", () => {
+   VANTA.BIRDS({
+     el: "#vanta",
+     mouseControls: true,
+     touchControls: true,
+     gyroControls: true,
+     minHeight: 200.0,
+     minWidth: 200.0,
+     scale: 1.0,
+     scaleMobile: 1.0,
+     backgroundAlpha: 0.0,
+     mouseControls: true,
+     touchControls: true,
+     gyroControls: false,
+     minHeight: 200.0,
+     minWidth: 200.0,
+     scale: 1.0,
+     scaleMobile: 1.0,
+     color2: 0xa4ff,
+     colorMode: "variance",
+     wingSpan: 40.0,
+     separation: 100.0,
+     alignment: 77.00,
+     cohesion: 5.00,
+     quantity: 3.00
+   })
+})
